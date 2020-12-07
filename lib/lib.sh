635c98e8f15b8f95f2e0474ed45ad2a45d5b1d1d
Kirill Müller <krlmlr@mailbox.org>
2020-12-07 05:43:31 +0100

    Un-tab

--- lib/lib.sh
+++ lib/lib.sh
@@ -52,7 +52,7 @@ EOF
 }
 
 add_worktrees() { # Add all branches representing workflows in foreign repositories as worktrees in the wt/ directory
-	git branch -r | egrep -v 'main' | sed 's#  origin/##' | xargs -r -n 1 ./run.sh _add_worktree
+  git branch -r | egrep -v 'main' | sed 's#  origin/##' | xargs -r -n 1 ./run.sh _add_worktree
 }
 
 _add_worktree() {
@@ -71,7 +71,7 @@ _add_worktree() {
 
 wt_run() { # Run command in all worktrees, use '{}' as placeholder for worktree directory
   _provide_wt
-	find wt/*/* -maxdepth 0 -type d | parallel -q -I '{}' ./run.sh "$@"
+  find wt/*/* -maxdepth 0 -type d | parallel -q -I '{}' ./run.sh "$@"
 }
 
 _provide_wt() {
@@ -81,7 +81,7 @@ _provide_wt() {
 }
 
 wt_git() { # Run git command in all worktrees
-	wt_run _wtdir_git '{}' "$@"
+  wt_run _wtdir_git '{}' "$@"
 }
 
 _wtdir_git() {
@@ -93,35 +93,35 @@ _wtdir_git() {
 }
 
 wt_pull() { # Run git pull --ff-only for all worktrees
-	wt_git pull --ff-only
+  wt_git pull --ff-only
 }
 
 copy_templates() { # Copy workflow templates into foreign repository
-	rm -rf wt/*/*/.github
-	wt_run _copy_template
+  rm -rf wt/*/*/.github
+  wt_run _copy_template
 }
 
 _copy_template() {
   echo "$1"
-	cp -r template/{*,.??*} "$1"
-	git -C "$1" add .
-	if git -C "$1" diff-index --quiet HEAD; then
+  cp -r template/{*,.??*} "$1"
+  git -C "$1" add .
+  if git -C "$1" diff-index --quiet HEAD; then
     git -C "$1" commit -m 'Update push action'
-	  git -C "$1" push -n
+    git -C "$1" push -n
   else
     echo "No changes"
   fi
 }
 
 remove_worktrees() { # Remove wt/ directory and all local branches. Potentially destructive, check output!
-	git worktree list | egrep -v '[[]main[]]' | cut -d " " -f 1 | xargs -r -n 1 git worktree remove
-	if [ -d $(ls -d wt/* | head -n 1) ]; then
+  git worktree list | egrep -v '[[]main[]]' | cut -d " " -f 1 | xargs -r -n 1 git worktree remove
+  if [ -d $(ls -d wt/* | head -n 1) ]; then
     rmdir wt/*
   fi
-	if [ -d wt ]; then
+  if [ -d wt ]; then
     rmdir wt
   fi
-	git branch | egrep -v 'main' | xargs -r git branch -d || git branch | egrep -v 'main' | xargs -r git branch -D
+  git branch | egrep -v 'main' | xargs -r git branch -d || git branch | egrep -v 'main' | xargs -r git branch -D
 }
 
 wt_merge_with() { # Merge a branch into all worktrees, don't commit. Pass local branch as argument
@@ -141,9 +141,9 @@ wt_finish_merge() { # Finish merging, push
 _wtdir_finish_merge() {
   git_dir="$1"
   shift
-	if git -C "$git_dir" diff-index --quiet HEAD; then
+  if git -C "$git_dir" diff-index --quiet HEAD; then
     git -C "$git_dir" commit --no-edit
-	  git -C "$git_dir" push
+    git -C "$git_dir" push
   fi
 }
 
