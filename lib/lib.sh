64769229c273ac86989de4b5cec584dba017d918
Kirill Müller <krlmlr@mailbox.org>
2020-12-07 06:05:20 +0100

    Push for real

--- lib/lib.sh
+++ lib/lib.sh
@@ -107,7 +107,7 @@ _copy_template() {
   git -C "$1" add .
   if git -C "$1" diff-index --quiet HEAD; then
     git -C "$1" commit -m 'Update push action'
-    git -C "$1" push -n
+    git -C "$1" push
   else
     echo "No changes"
   fi
