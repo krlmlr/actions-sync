f5afbccb71e5e95701aca3f84f06c7291d94502b
Kirill Müller <krlmlr@mailbox.org>
2020-12-07 17:47:53 +0100

    Avoid phone home, remove secrets

--- lib/lib.sh
+++ lib/lib.sh
@@ -103,6 +103,7 @@ wt_pull() { # Run git pull --ff-only for all worktrees
 
 copy_templates() { # Copy workflow templates into foreign repository
   rm -rf wt/*/*/.github
+  rm -rf wt/*/*/actions-subtree.yaml
   wt_run _copy_template
 }
 
