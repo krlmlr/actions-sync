9df60691f11651e6c4365acb7c9c3501bc4cbb93
Kirill Müller <kirill@cynkra.com>
2024-09-04 20:28:31 +0200

    Create PR instead of pushing

--- lib/lib.sh
+++ lib/lib.sh
@@ -315,7 +315,10 @@ merge_into_remote() { # Merge our workflow into the remote repository. Makes wor
           git commit -m "ci: Import from actions-sync, check carefully"
         fi
       fi
-      git push
+      git branch -m actions-sync
+      git push -u origin HEAD -f
+      gh pr create --fill-first
+      gh pr --merge --squash --auto
     else
       echo "Nothing to cherry-pick"
     fi
